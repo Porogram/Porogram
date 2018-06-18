6cefcf54e21d0c9be947cc302b9f540073dd4c8b
Jewon Oh <joh0531@gmail.com>
2018-06-18 15:12:44 -0400

    search wip

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 // import './search.css';
 
 class Search extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { sumName: "" };
+  }
+
   render() {
     return (
-      <form>
-        <input type="search" />
-        <button>Search</button>
-      </form>
+      <div className="search">
+        <input type="search" value={this.state.sumName} onChange={event => this.onInputChange(event.target.value)} />
+        <button onClick={() => this.onClick(this.state.sumName)}>Search</button>
+      </div>
     );
   }
+
+  onInputChange(sumName) {
+      this.setState({ sumName });
+  }
+
+  onClick(sumName) {
+      // console.log(sumName);
+      this.props.onInputChange(sumName);
+  }
+
 }
 
 export default Search;
