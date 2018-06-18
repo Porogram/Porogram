0ae2346208fa9c4c2f931290eab9f10e80445555
Jewon Oh <joh0531@gmail.com>
2018-06-18 11:45:09 -0400

    fixed name conflict

---
+++ client/src/components/search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import './search.css';
+
+class Search extends Component {
+  render() {
+    return (
+      <form>
+        <input type="search" />
+        <button>Search</button>
+      </form>
+    );
+  }
+}
+
+export default Search;
