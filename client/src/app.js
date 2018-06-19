b5b28bdad4705c29d0efd32bde6c3c087d9fa812
mshan10 <mshan@nd.edu>
2018-06-18 20:49:01 -0400

    added styling to searchbar and headnav components

--- client/src/app.js
+++ client/src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './app.css';
 import Search from './components/search';
+import HeadNav from './components/head_nav';
 
 class App extends Component {
   constructor(props) {
@@ -16,13 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        // <header className="App-header">
-        //   <img src={logo} className="App-logo" alt="logo" />
-        //   <h1 className="App-title">Welcome to React</h1>
-        // </header>
-        // <p className="App-intro">
-        //   To get started, edit <code>src/App.js</code> and save to reload.
-        // </p>
+        <HeadNav/>
         <Search onInputChange={this.sumSearch}/>
       </div>
     );
---
+++ client/src/components/head_nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './head_nav.css';
+
+
+const HeadNav = (props) => {
+  return (
+    <nav className="navbar navbar-light">
+        <a href="#"><img src="./favicon.png"/>LOL Stats</a>
+    </nav>
+
+  )
+}
+
+export default HeadNav;
