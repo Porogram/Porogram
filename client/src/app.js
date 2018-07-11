1674a8f07d782c711115581fba9c9feca923776a
Jewon Oh <joh0531@gmail.com>
2018-07-11 10:33:06 -0400

    refactored to css baseline

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Navbar from './components/navbar';
 import Home from './components/home';
 import Summoner from './components/summoner';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/summoner/:summonerName" component={Summoner} />
-                </Switch>
-            </div>
-        </BrowserRouter>
+        <div className="App">
+            <CssBaseline />
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/summoner/:summonerName" component={Summoner} />
+            </Switch>
+        </div>
     );
 }
 
