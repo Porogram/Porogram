8df870e2cda2a4ce58e550c4e37da1ade9a4bf78
Edward Atkinson <eatkins1@nd.edu>
2018-07-19 18:25:33 -0400

    working on automated build bot messaged

--- bot.py
+++ bot.py
@@ -6,10 +6,13 @@ import sys
 BOT_ID = "8357d94d27da14ccdab39797ec"
 
 def sendMessage(text):
-    print(text)
-    requestData = {"bot_id": BOT_ID, "text": text}
-    r = requests.post("https://api.groupme.com/v3/bots/post", data = requestData)
-    print(r)
+    go = input(f"Are you sure you want to send the message? {text}\n")
+    if go == 'y':
+        requestData = {"bot_id": BOT_ID, "text": text}
+        r = requests.post("https://api.groupme.com/v3/bots/post", data = requestData)
+        print(r)
+    else:
+        print("Did not send message")
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
