84f3e4718525796857e0e7b46ac7c9dc85e3970f
Edward Atkinson <eatkins1@nd.edu>
2018-07-19 19:36:36 -0400

    take 42.1

--- server/bot.py
+++ server/bot.py
@@ -6,11 +6,9 @@ import sys
 BOT_ID = "8357d94d27da14ccdab39797ec"
 
 def sendMessage(text):
-    go = input(f"Are you sure you want to send the message? {text}\n")
-    if go == 'y':
-        requestData = {"bot_id": BOT_ID, "text": text}
-        r = requests.post("https://api.groupme.com/v3/bots/post", data = requestData)
-        print(r)
+    requestData = {"bot_id": BOT_ID, "text": text}
+    r = requests.post("https://api.groupme.com/v3/bots/post", data = requestData)
+    print(f"Sent message '{text}' to groupMe channel")
     else:
         print("Did not send message")
 
