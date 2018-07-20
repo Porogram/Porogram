dc4188a06aaa3ac9d0f4ae3442b8df2c4d1b459c
Jewon Oh <joh0531@gmail.com>
2018-07-20 13:39:51 -0400

    updated searchbar

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,4 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import Search from './search'
 
-export default () => <Search />
+const styles = theme => ({
+    search: {
+        width: '50%',
+        margin: '220px auto'
+    }
+})
+
+export default withStyles(styles)(props => {
+    const { classes } = props
+    return (
+        <Fragment>
+            <div className={classes.search}>
+                <Search />
+            </div>
+        </Fragment>
+    )
+})
--- client/src/components/Home/search.js
+++ client/src/components/Home/search.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
-import { withStyles } from '@material-ui/core/styles'
-import { Input, Button } from '@material-ui/core'
 import SearchBar from 'material-ui-search-bar'
 import XRegExp from 'xregexp'
 
-const styles = theme => ({
-    search: {
-        textAlign: 'center',
-        marginTop: 240
-    }
-})
-
-export default withStyles(styles)(class extends Component {
+export default class extends Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -27,26 +18,17 @@ export default withStyles(styles)(class extends Component {
             this.setState({ invalidInput: true })
     }
     render() {
-        const { classes } = this.props
         const { summonerName, toSummoner, invalidInput } = this.state
         if (toSummoner) return <Redirect to={`/summoner/${summonerName}/matches`} />
         return (
-            <div className={classes.search}>
+            <div>
                 <SearchBar
                     value={summonerName}
                     onChange={value => this.setState({ summonerName: value })}
                     onRequestSearch={this.onSearch}
                 />
-                <Button onClick={this.onSearch}>Search</Button>
                 {invalidInput && <h3>Invalid input</h3>}
             </div>
         )
     }
-})
-
-// <Input
-//     type="text"
-//     value={summonerName}
-//     placeholder="Summoner Name"
-//     onChange={event => this.setState({ summonerName: event.target.value })}
-// />
+}
