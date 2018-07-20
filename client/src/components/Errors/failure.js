4a06f2e5c6f7959108c4c5fb7e8b4b035291fca0
mshan10 <mshan@nd.edu>
2018-07-20 15:02:17 -0400

    added styling to error page

--- client/src/components/Errors/failure.js
+++ client/src/components/Errors/failure.js
@@ -1,11 +1,39 @@
 import React, { Fragment } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
+import dogeImage from '../../images/error-doge.jpg'
 
-export default ({ error }) => {
+const styles = theme => ({
+    error: {
+        textAlign: 'center',
+    },
+    failure: {
+        letterSpacing: .3,
+        transform: 'scaleY(1.2)',
+        marginTop: 20,
+    },
+    status: {
+        color: '#0080ff',
+        fontWeight: 400,
+        letterSpacing: .5,
+    },
+    notFound: {
+        margin: '10px auto',
+    },
+    doge: {
+        height: 300,
+        width: 'auto',
+    }
+})
+
+export default withStyles(styles)(props => {
+    const { classes, error } = props
     return (
-        <Fragment>
-            <h1>ERROR</h1>
-            {'status_code' in error && <h2>STATUS CODE: {error.status_code}</h2>}
-            {'message' in error && <h2>MESSAGE: {error.message}</h2>}
-        </Fragment>
+        <div className={classes.error}>
+            <Typography variant="display4" className={classes.failure}>ERROR</Typography>
+            {'status_code' in error && <Typography variant="subheading" className={classes.status}>STATUS CODE: {error.status_code}</Typography>}
+            {'message' in error && <Typography variant="subheading" className={classes.status}>MESSAGE: {error.message}</Typography>}
+            <img src={dogeImage} className={classes.doge}/>
+        </div>
     )
-}
+})
