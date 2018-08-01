7ff05760a56137a993d82eedb9475fa379fb1878
Jewon Oh <joh0531@gmail.com>
2018-07-31 20:46:51 -0400

    removed details.js

--- client/src/components/Summoner/Summary/index.js
+++ client/src/components/Summoner/Summary/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
-import Details from './details'
 
 export default withStyles(theme => ({
     main: {
@@ -27,9 +26,6 @@ export default withStyles(theme => ({
             <Typography variant="display2" className={classes.title}>
                 Summary Details
             </Typography>
-            <Details
-                summoner={summoner}
-            />
         </div>
     )
 })
