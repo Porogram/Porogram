e6ea8d5e03407e6b6854e62b61ebb463cc43b9f5
Jewon Oh <joh0531@gmail.com>
2018-08-05 00:37:39 -0400

    refactored search component

---
+++ client/src/components/Home/Search/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import SearchBar from './searchBar'
+
+export default withStyles(theme => ({
+    main: {
+        height: '90vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    search: {
+        [theme.breakpoints.up('md')]: {
+            width: 600
+        },
+        [theme.breakpoints.down('sm')]: {
+            width: 300
+        }
+    }
+}))(({ classes }) => {
+    return (
+        <div className={classes.main}>
+            <div className={classes.search}>
+                <SearchBar />
+            </div>
+        </div>
+    )
+})
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import Search from './search'
+import Search from './Search'
 
-export default withStyles(theme => ({
-    main: {
-        height: '90vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    search: {
-        [theme.breakpoints.up('md')]: {
-            width: 600
-        },
-        [theme.breakpoints.down('sm')]: {
-            width: 300
-        }
-    }
-}))(class extends Component {
+export default class extends Component {
     render() {
-        const { classes } = this.props
         return (
-            <div className={classes.main}>
-                <div className={classes.search}>
-                    <Search />
-                </div>
-            </div>
+            <Search />
         )
     }
-})
+}
