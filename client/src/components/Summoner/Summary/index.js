925188be047bcb98a1218e03176dbdf66233dbd5
Jewon Oh <joh0531@gmail.com>
2018-08-09 20:59:42 -0400

    got summoner page to show with context

--- client/src/components/Summoner/Summary/index.js
+++ client/src/components/Summoner/Summary/index.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { Avatar, Typography, Paper } from '@material-ui/core'
+import {
+    Avatar,
+    Typography,
+    Paper,
+    Grid,
+    CircularProgress
+} from '@material-ui/core'
+import { StaticDataContext, SummonerDataContext } from '../../Context'
 
 export default withStyles(theme => ({
     main: {
@@ -13,7 +20,6 @@ export default withStyles(theme => ({
         marginBottom: -theme.spacing.unit * 25,
         color: '#fff'
     },
-
     Avatar: {
         width: theme.spacing.unit * 18,
         height: theme.spacing.unit * 18,
@@ -29,55 +35,138 @@ export default withStyles(theme => ({
         borderRadius: 0,
         height: '100vh',
         padding: theme.spacing.unit * 8
+    },
+    circularProgress: {
+        height: '80vh'
+    },
+// }))(({
+//     classes,
+//     summoner,
+//     positions,
+//     championMasteries,
+//     matchlist,
+//     matches,
+//     staticData
+// }) => {
+//     const { name, profileIconId, summonerLevel } = summoner
+//     const { tier, rank, leaguePoints, wins, losses } = positions
+//     const { version, champions } = staticData
+//     const baseUrl = 'https://ddragon.leagueoflegends.com/'
+//     const champion = Object.values(champions).find(champion =>
+//         matchlist.matches[0].champion === parseInt(champion.key, 10)).id
+//     const background = {backgroundImage: `url(${baseUrl}cdn/img/champion/splash/${champion}_0.jpg)`}
+//     return (
+//         <div className={classes.main} style={background}>
+//             {name && <Typography variant='display3' align='center' className={classes.summonerName}>{name}</Typography>}
+//             <div className={classes.info}>
+//                 {version && profileIconId && (
+//                     <Avatar
+//                         src={`${baseUrl}cdn/${version}/img/profileicon/${profileIconId}.png`}
+//                         alt=""
+//                         className={classes.Avatar}
+//                     />
+//                 )}
+//                 <Paper className={classes.infoPage} elevation={20}>
+//                     {summonerLevel && (
+//                         <Typography variant='subheading'>
+//                             Level: {summonerLevel}
+//                         </Typography>
+//                     )}
+//                     {tier && rank && (
+//                         <Typography variant='subheading'>
+//                             {`${tier} ${rank}`}
+//                         </Typography>
+//                     )}
+//                     <Typography variant='subheading'>
+//                         {`LP: ${leaguePoints}`}
+//                     </Typography>
+//                     {wins && losses && (
+//                         <Typography variant='subheading'>
+//                             {`W${wins} : L${losses}`}
+//                         </Typography>
+//                     )}
+//                 </Paper>
+//             </div>
+//         </div>
+//     )
+// })
+}))(class extends Component {
+    componentDidMount() {
+        this.props.getSummonerData(this.props.summonerName)
     }
-}))(({
-    classes,
-    summoner,
-    positions,
-    championMasteries,
-    matchlist,
-    matches,
-    staticData
-}) => {
-    const { name, profileIconId, summonerLevel } = summoner
-    const { tier, rank, leaguePoints, wins, losses } = positions
-    const { version, champions } = staticData
-    const baseUrl = 'https://ddragon.leagueoflegends.com/'
-    const champion = Object.values(champions).find(champion =>
-        matchlist.matches[0].champion === parseInt(champion.key, 10)).id
-    const background = {backgroundImage: `url(${baseUrl}cdn/img/champion/splash/${champion}_0.jpg)`}
-    return (
-        <div className={classes.main} style={background}>
-            {name && <Typography variant='display3' align='center' className={classes.summonerName}>{name}</Typography>}
-            <div className={classes.info}>
-                {version && profileIconId && (
-                    <Avatar
-                        src={`${baseUrl}cdn/${version}/img/profileicon/${profileIconId}.png`}
-                        alt=""
-                        className={classes.Avatar}
-                    />
+    render() {
+        const { classes } = this.props
+        return (
+            <SummonerDataContext.Consumer>
+                {({
+                    state: {
+                        fetchedData,
+                        summoner,
+                        positions,
+                        championMasteries,
+                        matchlist,
+                        matches
+                    }
+                }) => (
+                    <Fragment>
+                        {!fetchedData && (
+                            <Grid
+                                container
+                                alignItems="center"
+                                justify="center"
+                                className={classes.circularProgress}
+                            >
+                                <CircularProgress />
+                            </Grid>
+                        )}
+                        {fetchedData && (
+                            <StaticDataContext.Consumer>
+                                {({ state: { version, champions }, baseUrl }) => {
+                                    const { name, profileIconId, summonerLevel } = summoner
+                                    const { tier, rank, leaguePoints, wins, losses } = positions
+                                    const champion = Object.values(champions).find(champion =>
+                                        matchlist.matches[0].champion === parseInt(champion.key, 10)).id
+                                    const background = {backgroundImage: `url(${baseUrl}/cdn/img/champion/splash/${champion}_0.jpg)`}
+                                    return (
+                                        <div className={classes.main} style={background}>
+                                            {name && <Typography variant='display3' align='center' className={classes.summonerName}>{name}</Typography>}
+                                            <div className={classes.info}>
+                                                {version && profileIconId && (
+                                                    <Avatar
+                                                        src={`${baseUrl}/cdn/${version}/img/profileicon/${profileIconId}.png`}
+                                                        alt=""
+                                                        className={classes.Avatar}
+                                                    />
+                                                )}
+                                                <Paper className={classes.infoPage} elevation={20}>
+                                                    {summonerLevel && (
+                                                        <Typography variant='subheading'>
+                                                            Level: {summonerLevel}
+                                                        </Typography>
+                                                    )}
+                                                    {tier && rank && (
+                                                        <Typography variant='subheading'>
+                                                            {`${tier} ${rank}`}
+                                                        </Typography>
+                                                    )}
+                                                    <Typography variant='subheading'>
+                                                        {`LP: ${leaguePoints}`}
+                                                    </Typography>
+                                                    {wins && losses && (
+                                                        <Typography variant='subheading'>
+                                                            {`W${wins} : L${losses}`}
+                                                        </Typography>
+                                                    )}
+                                                </Paper>
+                                            </div>
+                                        </div>
+                                    )
+                                }}
+                            </StaticDataContext.Consumer>
+                        )}
+                    </Fragment>
                 )}
-                <Paper className={classes.infoPage} elevation={20}>
-                    {summonerLevel && (
-                        <Typography variant='subheading'>
-                            Level: {summonerLevel}
-                        </Typography>
-                    )}
-                    {tier && rank && (
-                        <Typography variant='subheading'>
-                            {`${tier} ${rank}`}
-                        </Typography>
-                    )}
-                    <Typography variant='subheading'>
-                        {`LP: ${leaguePoints}`}
-                    </Typography>
-                    {wins && losses && (
-                        <Typography variant='subheading'>
-                            {`W${wins} : L${losses}`}
-                        </Typography>
-                    )}
-                </Paper>
-            </div>
-        </div>
-    )
+            </SummonerDataContext.Consumer>
+        )
+    }
 })
--- client/src/components/Summoner/index.js
+++ client/src/components/Summoner/index.js
@@ -3,103 +3,114 @@ import { CircularProgress } from '@material-ui/core'
 import axios from 'axios'
 import Summary from './Summary'
 import { Failure } from '../Errors'
+import { SummonerDataContext } from '../Context'
 
-export default class extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            fetchedData: false,
-            summoner: {},
-            positions: {},
-            championMasteries: {},
-            matchlist: {},
-            matches: [],
-            version: '',
-            champions: {},
-            summonerSpells: {},
-            runes: {},
-            items: {},
-            error: {}
-        }
-    }
-    componentDidMount() {
-        Promise.all([this.getSummonerData(), this.getStaticData()])
-            .then(() => this.setState({ fetchedData: true }))
-    }
-    getSummonerData = () => {
-        return axios.get(`/api/search/${this.props.match.params.summonerName}`)
-            .then(res =>
-                this.setState({
-                    summoner: 'summoner' in res.data && res.data.summoner,
-                    positions:
-                        'positions' in res.data &&
-                        res.data.positions.length &&
-                        res.data.positions[0],
-                    championMasteries:
-                        'championMasteries' in res.data &&
-                        res.data.championMasteries,
-                    matchlist: 'matchlist' in res.data && res.data.matchlist,
-                    matches: 'matches' in res.data && res.data.matches
-                })
-            ).catch(error =>
-                this.setState({
-                    error: { message: 'Failed to get summoner data' }
-                })
-            )
-    }
-    getStaticData = () => {
-        const baseUrl = 'https://ddragon.leagueoflegends.com/'
-        return axios.get(`${baseUrl}api/versions.json`)
-            .then(({ data }) =>
-                Promise.all([
-                    data[0],
-                    axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/champion.json`),
-                    axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/summoner.json`),
-                    axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/runesReforged.json`),
-                    axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/item.json`)
-                ])
-            ).then(([version, champions, summonerSpells, runes, items]) =>
-                this.setState({
-                    version,
-                    champions: champions.data.data,
-                    summonerSpells: summonerSpells.data.data,
-                    runes: runes.data,
-                    items: items.data.data
-                })
-            ).catch(error =>
-                this.setState({
-                    error: { message: 'Failed to get static data' }
-                })
-            )
-    }
-    render() {
-        const {
-            fetchedData,
-            summoner,
-            positions,
-            championMasteries,
-            matchlist,
-            matches,
-            version,
-            champions,
-            summonerSpells,
-            runes,
-            items,
-            error
-        } = this.state
-        const staticData = { version, champions, summonerSpells, runes, items }
-        if (!fetchedData) return <CircularProgress />
-        if ('message' in error) return <Failure error={error} />
-        else if ('message' in summoner) return <Failure error={summoner} />
-        return (
+// export default class extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             fetchedData: false,
+//             summoner: {},
+//             positions: {},
+//             championMasteries: {},
+//             matchlist: {},
+//             matches: [],
+//             version: '',
+//             champions: {},
+//             summonerSpells: {},
+//             runes: {},
+//             items: {},
+//             error: {}
+//         }
+//     }
+//     componentDidMount() {
+//         Promise.all([this.getSummonerData(), this.getStaticData()])
+//             .then(() => this.setState({ fetchedData: true }))
+//     }
+//     getSummonerData = () => {
+//         return axios.get(`/api/search/${this.props.match.params.summonerName}`)
+//             .then(res =>
+//                 this.setState({
+//                     summoner: 'summoner' in res.data && res.data.summoner,
+//                     positions:
+//                         'positions' in res.data &&
+//                         res.data.positions.length &&
+//                         res.data.positions[0],
+//                     championMasteries:
+//                         'championMasteries' in res.data &&
+//                         res.data.championMasteries,
+//                     matchlist: 'matchlist' in res.data && res.data.matchlist,
+//                     matches: 'matches' in res.data && res.data.matches
+//                 })
+//             ).catch(error =>
+//                 this.setState({
+//                     error: { message: 'Failed to get summoner data' }
+//                 })
+//             )
+//     }
+//     getStaticData = () => {
+//         const baseUrl = 'https://ddragon.leagueoflegends.com/'
+//         return axios.get(`${baseUrl}api/versions.json`)
+//             .then(({ data }) =>
+//                 Promise.all([
+//                     data[0],
+//                     axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/champion.json`),
+//                     axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/summoner.json`),
+//                     axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/runesReforged.json`),
+//                     axios.get(`${baseUrl}cdn/${data[0]}/data/en_US/item.json`)
+//                 ])
+//             ).then(([version, champions, summonerSpells, runes, items]) =>
+//                 this.setState({
+//                     version,
+//                     champions: champions.data.data,
+//                     summonerSpells: summonerSpells.data.data,
+//                     runes: runes.data,
+//                     items: items.data.data
+//                 })
+//             ).catch(error =>
+//                 this.setState({
+//                     error: { message: 'Failed to get static data' }
+//                 })
+//             )
+//     }
+//     render() {
+//         const {
+//             fetchedData,
+//             summoner,
+//             positions,
+//             championMasteries,
+//             matchlist,
+//             matches,
+//             version,
+//             champions,
+//             summonerSpells,
+//             runes,
+//             items,
+//             error
+//         } = this.state
+//         const staticData = { version, champions, summonerSpells, runes, items }
+//         if (!fetchedData) return <CircularProgress />
+//         if ('message' in error) return <Failure error={error} />
+//         else if ('message' in summoner) return <Failure error={summoner} />
+//         return (
+//             <Summary
+//                 summoner={summoner}
+//                 positions={positions}
+//                 championMasteries={championMasteries}
+//                 matchlist={matchlist}
+//                 matches={matches}
+//                 staticData={staticData}
+//             />
+//         )
+//     }
+// }
+
+export default ({ match: { params: { summonerName } } }) => (
+    <SummonerDataContext.Consumer>
+        {({ getSummonerData }) => (
             <Summary
-                summoner={summoner}
-                positions={positions}
-                championMasteries={championMasteries}
-                matchlist={matchlist}
-                matches={matches}
-                staticData={staticData}
-            />
-        )
-    }
-}
+                summonerName={summonerName} 
+                getSummonerData={getSummonerData} />
+        )}
+    </SummonerDataContext.Consumer>
+)
