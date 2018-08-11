9061b59fbe0d7ee40dfe11bb26664d9118b44611
Jewon Oh <joh0531@gmail.com>
2018-08-10 23:54:50 -0400

    made search component

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,46 +1,18 @@
 import React, { Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core'
-import Matches from './Matches'
-import SearchBar from './searchBar'
 import { SummonerDataContext } from '../Context'
+import Matches from './Matches'
+import Search from '../Search'
 import Loading from '../Layout/loading'
 
-export default withStyles(theme => ({
-    search: {
-        [theme.breakpoints.down('sm')]: {
-            height: `calc(100vh - 56px)`
-        } ,
-        [theme.breakpoints.up('sm')]: {
-            height: `calc(100vh - 64px)`
-        }
-    },
-    searchBar: {
-        [theme.breakpoints.down('sm')]: {
-            width: 300
-        },
-        [theme.breakpoints.up('sm')]: {
-            width: 600
-        }
-    }
-}))(({ classes }) => (
+export default () => (
     <SummonerDataContext.Consumer>
         {({
             state: { searched, fetchedData, summoner, matchlist, matches }
         }) => (
             <Fragment>
-                {!searched && (
-                    <Grid
-                        container
-                        alignItems="center"
-                        justify="center"
-                        className={classes.search}
-                    >
-                        <div className={classes.searchBar}>
-                            <SearchBar />
-                        </div>
-                    </Grid>
-                )}
+                {!searched && <Search />}
                 {searched && !fetchedData && <Loading />}
                 {fetchedData && (
                     <Matches
@@ -52,4 +24,4 @@ export default withStyles(theme => ({
             </Fragment>
         )}
     </SummonerDataContext.Consumer>
-))
+)
