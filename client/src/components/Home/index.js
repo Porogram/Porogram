b2b8419e6fdf61852085f7c47a909ac472975018
Jewon Oh <joh0531@gmail.com>
2018-08-15 22:30:59 -0400

    forgot what I did

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,34 +1,16 @@
 import React, { Fragment } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import { Grid } from '@material-ui/core'
 import { SummonerDataContext } from '../Context'
-import Loading from '../Layout/loading'
+import Loading from '../loading'
 import Search from '../Search'
 import Matches from './Matches'
 
 export default () => (
     <SummonerDataContext.Consumer>
-        {({
-            state: {
-                searched,
-                fetchedData,
-                summoner,
-                positions,
-                matchlist,
-                matches
-            }
-        }) => (
+        {({ state: { searched, fetchedData } }) => (
             <Fragment>
                 {!searched && <Search />}
                 {searched && !fetchedData && <Loading />}
-                {fetchedData && (
-                    <Matches
-                        summoner={summoner}
-                        positions={positions}
-                        matchlist={matchlist}
-                        matches={matches}
-                    />
-                )}
+                {fetchedData && <Matches />}
             </Fragment>
         )}
     </SummonerDataContext.Consumer>
