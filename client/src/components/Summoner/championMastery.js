09ba3b05f0b65676ca344f9219c53351602f2995
mshan10 <mshan@nd.edu>
2018-08-24 23:21:28 -0400

    fixed champion mastery and added champ images

--- client/src/components/Summoner/championMastery.js
+++ client/src/components/Summoner/championMastery.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Avatar, Paper, Typography, Grid } from '@material-ui/core'
 import { StaticDataContext } from '../Context'
-import four from '../../images/champion-mastery/4.png'
-import five from '../../images/champion-mastery/5.png'
-import six from '../../images/champion-mastery/6.png'
-import seven from '../../images/champion-mastery/7.png'
+import four from '../../images/champion-mastery/champion-master-4.png'
+import five from '../../images/champion-mastery/champion-master-5.png'
+import six from '../../images/champion-mastery/champion-master-6.png'
+import seven from '../../images/champion-mastery/champion-master-7.png'
 
 export default withStyles((theme) => ({
     title: {
@@ -38,19 +38,22 @@ export default withStyles((theme) => ({
                 <Fragment>
                 <Paper className={classes.paper}>
                     <Typography variant="headline" className={classes.title}>Champion Mastery</Typography>
-                    <Grid container direction="column">
-                        {[...Array(5).keys()].map(i => (
-                            <Grid item key={i} >
-                                {(championMasteries[i].championLevel > 3) ?
-                                    <Grid container direction="row" justify="space-between" alignItems="center">
-                                        <Grid item>
-                                            {getMasteryImage(`${championMasteries[i].championLevel}`)}
+                    <Grid container direction="column" justify="space-between" spacing={8}>
+                        {championMasteries.slice(0,5).map(championMastery => (
+                            <Grid item key={championMastery.championId}>
+                                {(championMastery.championLevel > 3) ?
+                                    <Grid container direction="row" justify="space-between" alignItems="center" spacing={32}>
+                                        <Grid item xs={2}>
+                                            {getMasteryImage(`${championMastery.championLevel}`)}
                                         </Grid>
-                                        <Grid item>
-                                            <Typography variant="subheading">{champions[championMasteries[i].championId].id}</Typography>
+                                        <Grid item xs={2}>
+                                            <Avatar src={`${baseUrl}/cdn/${version}/img/champion/${champions[championMastery.championId].image.full}`}/>
                                         </Grid>
-                                        <Grid item>
-                                            <Typography variant="subheading">{championMasteries[i].championPoints}</Typography>
+                                        <Grid item xs={5}>
+                                            <Typography variant="subheading">{champions[championMastery.championId].name}</Typography>
+                                        </Grid>
+                                        <Grid item xs={3}>
+                                            <Typography variant="subheading">{championMastery.championPoints}</Typography>
                                         </Grid>
                                     </Grid>
                                 :(<Fragment/>)}
