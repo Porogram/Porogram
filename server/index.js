b49b445de8cd824b36c8ca3e3400b64f2f2c93d4
Jewon Oh <joh0531@gmail.com>
2018-09-09 01:14:42 -0400

    connected to remote db

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const summoner = require('./routes/summoner')
 const match = require('./routes/match')
 const matches = require('./routes/matches')
 
-mongoose.connect('mongodb://localhost/porogram', { useNewUrlParser: true })
+mongoose.connect(
+    'mongodb://jewonoh:porogram@ec2-18-191-173-27.us-east-2.compute.amazonaws.com/porogram',
+    { auth: { authdb: 'admin' }, useNewUrlParser: true }
+)
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => console.log('connected to database'))
