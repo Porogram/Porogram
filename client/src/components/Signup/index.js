2a429d155383f4aadc2436b3679556fccb099c6f
Jewon Oh <joh0531@gmail.com>
2018-09-10 09:53:35 -0400

    save values from textfields

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Button, Grid, Paper, TextField, Typography } from '@material-ui/core'
 
@@ -19,48 +19,101 @@ export default withStyles(theme => ({
             height: 'calc(100vh - 64px)'
         }
     }
-}))(({ classes }) => (
-    <Grid
-        alignItems="center"
-        className={classes.signup}
-        container
-        direction="column"
-        justify="center"
-    >
-        <Grid item>
-            <Paper className={classes.paper}>
-                <Grid
-                    alignItems="center"
-                    container
-                    direction="column"
-                    justify="center"
-                >
-                    <Grid item>
-                        <TextField label="First name" />
-                    </Grid>
-                    <Grid item>
-                        <TextField label="Last name" />
-                    </Grid>
-                    <Grid item>
-                        <TextField label="Email" />
-                    </Grid>
-                    <Grid item>
-                        <TextField label="Password" />
-                    </Grid>
-                    <Grid item>
-                        <TextField label="Summoner name" />
-                    </Grid>
-                    <Grid item>
-                        <Button
-                            className={classes.button}
-                            color="primary"
-                            variant="contained"
+}))(class extends Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        summonerName: ''
+    }
+    onClick = () => {
+        const { firstName, lastName, email, password, summonerName } = this.state
+        console.log('firstName', firstName)
+        console.log('lastName', lastName)
+        console.log('email', email)
+        console.log('password', password)
+        console.log('summonerName', summonerName)
+    }
+    render() {
+        const { classes } = this.props
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            summonerName
+        } = this.state
+        return (
+            <Grid
+                alignItems="center"
+                className={classes.signup}
+                container
+                direction="column"
+                justify="center"
+            >
+                <Grid item>
+                    <Paper className={classes.paper}>
+                        <Grid
+                            alignItems="center"
+                            container
+                            direction="column"
+                            justify="center"
                         >
-                            SIGN UP
-                        </Button>
-                    </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="First name"
+                                    onChange={e =>
+                                        this.setState({ firstName: e.target.value })
+                                    }
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="Last name"
+                                    onChange={e =>
+                                        this.setState({ lastName: e.target.value })
+                                    }
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="Email"
+                                    onChange={e =>
+                                        this.setState({ email: e.target.value })
+                                    }
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="Password"
+                                    onChange={e =>
+                                        this.setState({ password: e.target.value })
+                                    }
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="Summoner name"
+                                    onChange={e =>
+                                        this.setState({ summonerName: e.target.value })
+                                    }
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    className={classes.button}
+                                    color="primary"
+                                    onClick={this.onClick}
+                                    variant="contained"
+                                >
+                                    SIGN UP
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
                 </Grid>
-            </Paper>
-        </Grid>
-    </Grid>
-))
+            </Grid>
+        )
+    }
+})
