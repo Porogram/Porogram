0349f89c67e45a23bfb3f03f3d78a8a9cb09fca6
Jewon Oh <joh0531@gmail.com>
2018-09-11 17:14:02 -0400

    fixed database warning

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ mongoose.connect(
     'mongodb://jewonoh:porogram@ec2-18-191-173-27.us-east-2.compute.amazonaws.com/porogram',
     { auth: { authdb: 'admin' }, useNewUrlParser: true }
 )
+mongoose.set('useCreateIndex', true)
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => console.log('connected to database'))
