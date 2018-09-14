4579b68d700172001526715fd77b597be5a01756
Jewon Oh <joh0531@gmail.com>
2018-09-14 00:52:44 -0400

    updated route formatting

--- server/routes/summoner.js
+++ server/routes/summoner.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const rp = require('request-promise')
 const utils = require('../utils')
 
 router.use((req, res, next) => {
@@ -11,33 +12,41 @@ router.use((req, res, next) => {
 })
 
 router.get('/:summonerName', (req, res) => {
-    utils.request(
-        utils.createUrl(
-            '/summoner/v3/summoners/by-name',
-            req.params.summonerName
-        )
-    ).then(summoner =>
+    rp({
+        json: true,
+        uri:
+            utils.createUrl(
+                '/summoner/v3/summoners/by-name',
+                req.params.summonerName
+            )
+    }).then(summoner =>
         Promise.all([
             summoner,
-            utils.request(
-                utils.createUrl(
-                    '/league/v3/positions/by-summoner',
-                    summoner.id
-                )
-            ),
-            utils.request(
-                utils.createUrl(
-                    '/champion-mastery/v3/champion-masteries/by-summoner',
-                    summoner.id
-                )
-            ),
-            utils.request(
-                utils.createUrl(
-                    '/match/v3/matchlists/by-account',
-                    summoner.accountId,
-                    { beginIndex: 0, endIndex: 10 }
-                )
-            )
+            rp({
+                json: true,
+                uri:
+                    utils.createUrl(
+                        '/league/v3/positions/by-summoner',
+                        summoner.id
+                    )
+            }),
+            rp({
+                json: true,
+                uri:
+                    utils.createUrl(
+                        '/champion-mastery/v3/champion-masteries/by-summoner',
+                        summoner.id
+                    )
+            }),
+            rp({
+                json: true,
+                uri:
+                    utils.createUrl(
+                        '/match/v3/matchlists/by-account',
+                        summoner.accountId,
+                        { beginIndex: 0, endIndex: 10 }
+                    )
+            })
         ])
     ).then(([summoner, positions, championMasteries, matchlist]) =>
         res.send({ summoner, positions, championMasteries, matchlist })
