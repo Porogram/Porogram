12f0ffcfd3aa62ef47837683f99700c1a1da2497
Jewon Oh <joh0531@gmail.com>
2018-09-19 10:32:48 -0400

    added logout feature

--- client/src/components/Layout/navbar.js
+++ client/src/components/Layout/navbar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import {
+    AppBar,
+    IconButton,
+    Menu,
+    MenuItem,
+    Toolbar,
+    Typography
+} from '@material-ui/core'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import { Consumer } from '../context'
 import logo from '../../images/logo/poro.png'
+
+const ITEM_HEIGHT = 48
 
 export default withStyles(theme => ({
     appBar: {
@@ -17,22 +27,66 @@ export default withStyles(theme => ({
         marginLeft: theme.spacing.unit * 2,
         marginRight: theme.spacing.unit * 2
     }
-}))(({ classes }) => (
-    <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-            <Link to="/" className={classes.logo}>
-                <img src={logo} alt="" className={classes.img} />
-            </Link>
-            <Typography variant="title" color="inherit" noWrap>
-                POROGRAM
-            </Typography>
-            <Consumer>
-                {({ logout }) => (
-                    <Link onClick={logout} to="/">
-                        <Typography variant="caption">LOGOUT</Typography>
-                    </Link>
-                )}
-            </Consumer>
-        </Toolbar>
-    </AppBar>
-))
+}))(class extends Component {
+    state = { anchorEl: null }
+    handleClick = e => {
+        this.setState({ anchorEl: e.currentTarget })
+    }
+    handleClose = () => {
+        this.setState({ anchorEl: null })
+    }
+    handleLogout = (e, logout) => {
+        e.preventDefault()
+        Promise.all([logout(), this.setState({ anchorEl: null })])
+    }
+    render() {
+        const { anchorEl } = this.state
+        const { classes } = this.props
+        return (
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Consumer>
+                        {({ state: { isAuthenticated }, logout }) => (
+                            <Fragment>
+                                <Link to="/" className={classes.logo}>
+                                    <img src={logo} alt="" className={classes.img} />
+                                </Link>
+                                <Typography variant="title" color="inherit" noWrap>
+                                    POROGRAM
+                                </Typography>
+                                {isAuthenticated && (
+                                    <IconButton
+                                        aria-label="More"
+                                        aria-owns={!!anchorEl ? 'long-menu' : null}
+                                        aria-haspopup="true"
+                                        onClick={this.handleClick}
+                                    >
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                )}
+                                <Menu
+                                    id="long-menu"
+                                    anchorEl={anchorEl}
+                                    open={!!anchorEl}
+                                    onClose={this.handleClose}
+                                    PaperProps={{
+                                        style: {
+                                            maxHeight: ITEM_HEIGHT * 4.5,
+                                            width: 200,
+                                        },
+                                    }}
+                                >
+                                    <MenuItem
+                                        onClick={e => this.handleLogout(e, logout)}
+                                    >
+                                        LOGOUT
+                                    </MenuItem>
+                                </Menu>
+                            </Fragment>
+                        )}
+                    </Consumer>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+})
